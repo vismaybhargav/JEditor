4934b41c68ec9f6aadaed70e39ac02b3f7e56881
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-06-14 18:44:36 -0700

    Implemented file tree building on workspace initialization

--- src/jeditor/components/explorer/FileExplorer.java
+++ src/jeditor/components/explorer/FileExplorer.java
@@ -2,8 +2,33 @@ package jeditor.components.explorer;
 
 import javafx.scene.control.TreeView;
 
+import java.io.File;
+
 public class FileExplorer extends TreeView<String> {
     public FileExplorer() {
         super();
+    }
+
+    public static ExplorerItem buildFileTree(String filePath) {
+        File rootFile = new File(filePath);
+        ExplorerItem root = new ExplorerItem(rootFile);
+
+        buildTree(root);
+        return root;
+    }
+
+    private static void buildTree(ExplorerItem parentItem) {
+        if (parentItem.isDirectory()) {
+            File[] files = parentItem.getFile().listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    ExplorerItem item = new ExplorerItem(file);
+                    item.expandedProperty().addListener(e -> item.updateExpandedGraphic());
+                    parentItem.getChildren().add(item);
+                    buildTree(item);
+                }
+            }
+        }
     }
 }
