918e0197b6c1f23c8b68fc2f71117ab986e6a2f0
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-06-14 18:45:50 -0700

    Added workspace opening functionality in the MenuBar

--- src/jeditor/components/TopMenu.java
+++ src/jeditor/components/TopMenu.java
@@ -3,11 +3,15 @@ package jeditor.components;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import jeditor.components.editorview.EditorInstance;
+import jeditor.components.explorer.ExplorerItem;
+import jeditor.components.explorer.FileExplorer;
 import jeditor.core.ComponentStorage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class TopMenu extends MenuBar {
     public TopMenu() {
@@ -15,19 +19,44 @@ public class TopMenu extends MenuBar {
 
         Menu fileMenu = new Menu("File");
         getMenus().add(fileMenu);
+
         MenuItem newFile = new MenuItem("New");
         MenuItem saveFile = new MenuItem("Save");
-        MenuItem openFile = new MenuItem("Open");
 
-        openFile.setOnAction(e -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Open File");
-            File requestedFile = fileChooser.showOpenDialog(null);
-            if(requestedFile == null) return;
-            ComponentStorage.getTabbedEditorPane().addEditorInstance(new EditorInstance(requestedFile, false));
-        });
+        MenuItem openFile = new MenuItem("Open");
+        openFile.setOnAction(e -> openFileControl());
 
         MenuItem openFolder = new MenuItem("Open Folder");
+        openFolder.setOnAction(e -> openFolderControl());
+
         fileMenu.getItems().addAll(newFile, saveFile, openFile, openFolder);
+    }
+
+    private void openFolderControl() {
+        DirectoryChooser dc = new DirectoryChooser();
+        File selectedDirectory = dc.showDialog(null);
+
+        try {
+            // build the file tree
+            ExplorerItem root = FileExplorer.buildFileTree(selectedDirectory.getCanonicalPath());
+
+            // checking for if the directory has been expanded
+            root.setExpanded(true);
+            root.updateExpandedGraphic();
+            root.expandedProperty().addListener(e -> root.updateExpandedGraphic());
+
+            ComponentStorage.getExplorer().setRoot(root);
+        } catch (IOException e) {
+            System.out.println("error building file tree" + e.getMessage());
+        }
+    }
+
+    private void openFileControl() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        File requestedFile = fileChooser.showOpenDialog(null);
+        if (requestedFile == null)
+            return;
+        ComponentStorage.getTabbedEditorPane().addEditorInstance(new EditorInstance(requestedFile, false));
     }
 }
